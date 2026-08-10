e37ec371ed714f272ea909090cd86c397d8a5ce4
Z User <z@container>
2026-08-10 19:44:50 +0000

    Deploy full Vite site from live capture

--- artifacts/erp-system/src/pages/employees/salary-modal/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Salary modal module barrel export.
- */
-export { fmt, DEDUCTION_LABELS, dedLabel, blankSettleLine } from './helpers';
-export { useSalaryMutations } from './useSalaryMutations';
